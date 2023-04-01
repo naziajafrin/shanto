83a5232e6877cfa488fdcd10506b5e8cb3f24c9b
Nazia <nazia.jafrin1@gmail.com>
2023-04-01 21:33:57 +0600

    shanto

---
+++ sigma_class.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+class employee{
+
+    private:
+    char name;
+    int age;
+    char company;
+
+    public:
+
+    void getset( char a, int i, char c){
+        name = a;
+        age = i;
+        company = c;
+
+        
+    }
+
+    void getInput(){
+        
+        cin >> name;
+        cin >> age;
+        cin >> company;
+
+    }
+
+
+    void Display(){
+        cout << "name:" << endl;
+        
+        cout << "age:" << endl;
+        
+        cout << "company:" << endl;
+        
+
+    }
+
+};
+
+int main(){
+
+    employee e1,e2;
+
+    e1.getInput();
+    e2.getInput();
+
+    e1.Display();
+    e2.Display();
+}
